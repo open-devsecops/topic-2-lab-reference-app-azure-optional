eae49bbe2952aa6de1960a771b57b382baa496e1
kiki3177 <150294655+kiki3177@users.noreply.github.com>
2025-05-26 01:44:54 -0700

    update secrets template for student use

---
+++ back_end/secrets.js
@@ -0,0 +1,10 @@
+const MapboxToken = {
+    tokenKey: "*****REPLACE WITH YOUR ACTUAL TOKEN KEY*****"
+}
+
+
+const MongoDBUri = {
+    uri: "mongodb+srv://*****REPLACE WITH YOUR ACTUAL MONGODB URI*****"
+}
+
+module.exports = { MongoDBUri };
